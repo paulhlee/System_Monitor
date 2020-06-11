2675a8d34064cefb6663c7b4565417c5adf34c8c
root <paulhaolee@gmail.com>
2020-06-11 02:03:09 +0000

    hi

---
+++ build/CMakeFiles/monitor.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/System_Monitor/src/format.cpp" "/root/System_Monitor/build/CMakeFiles/monitor.dir/src/format.cpp.o"
+  "/root/System_Monitor/src/linux_parser.cpp" "/root/System_Monitor/build/CMakeFiles/monitor.dir/src/linux_parser.cpp.o"
+  "/root/System_Monitor/src/main.cpp" "/root/System_Monitor/build/CMakeFiles/monitor.dir/src/main.cpp.o"
+  "/root/System_Monitor/src/ncurses_display.cpp" "/root/System_Monitor/build/CMakeFiles/monitor.dir/src/ncurses_display.cpp.o"
+  "/root/System_Monitor/src/process.cpp" "/root/System_Monitor/build/CMakeFiles/monitor.dir/src/process.cpp.o"
+  "/root/System_Monitor/src/processor.cpp" "/root/System_Monitor/build/CMakeFiles/monitor.dir/src/processor.cpp.o"
+  "/root/System_Monitor/src/system.cpp" "/root/System_Monitor/build/CMakeFiles/monitor.dir/src/system.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -70,7 +70,17 @@ vector<int> LinuxParser::Pids() {
 float LinuxParser::MemoryUtilization() { return 0.0; }
 
 // TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() { 
+  long uptime, idletime;
+  string line;
+  std::ifstream stream(kProcDirectory + kUptimeFilename);
+  if (stream.is_open()){
+    std::getline(stream,line);
+    std::istringstream linestream(line);
+    linestream >> uptime >> idletime;
+  }
+  
+  return uptime; }
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
